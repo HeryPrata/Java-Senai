8c67c54e4011f331aa71311d8787b2d3a3c34440
HeryPrata <157115637+HeryPrata@users.noreply.github.com>
2026-04-10 20:45:47 -0300

    Java_aula

---
+++ Aula_1_1_java_.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hery.silva
+ *///import java.util.Scanner;
+public class Aula_1_1_java_ {
+    //Scanner scan = new Scanner(System.in);
+    
+    //System.out.println("Informe o numero do ano: ");
+    //mes = scan.NextInt();
+    
+}
