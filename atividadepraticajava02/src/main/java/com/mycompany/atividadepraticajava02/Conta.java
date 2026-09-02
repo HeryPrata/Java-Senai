f37691c9fb107092cae18a70e36d5dd32900cb8d
HeryPrata <157115637+HeryPrata@users.noreply.github.com>
2026-09-02 01:58:38 -0300

    Update Conta.java
    Added fields and methods for deposit and withdrawal functionality.

--- atividadepraticajava02/src/main/java/com/mycompany/atividadepraticajava02/Conta.java
+++ atividadepraticajava02/src/main/java/com/mycompany/atividadepraticajava02/Conta.java
@@ -2,12 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.atividadepraticajava02;
+package com.mycompany.atividadedepraticajava02;
 
 /**
  *
- * @author hery.silva
+ * @author lucas.areas
  */
 public class Conta {
     
+    int numero;
+    String nome_titular;
+    double saldo, valor;
+	
+    void depositar(double valor){
+        this.saldo = this.saldo + valor;
+    }
+
+    boolean sacar(double valor){
+        if (this.saldo >= valor){
+                this.saldo = this.saldo - valor;
+                return(true);
+        }
+        else{
+                return(false);
+        }
+    }
 }
