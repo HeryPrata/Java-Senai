f485532d95d7bc92eb06b664fd9cb520ea9966a1
HeryPrata <157115637+HeryPrata@users.noreply.github.com>
2026-09-02 01:54:23 -0300

    Add files via upload

---
+++ atividadepraticajava02/src/main/java/com/mycompany/atividadepraticajava02/Conta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atividadepraticajava02;
+
+/**
+ *
+ * @author hery.silva
+ */
+public class Conta {
+    
+}
